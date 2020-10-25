d2dc4aeb512725c45e7a8204434bc159f902f080
Erich Styger <erich.styger@hslu.ch>
2020-10-25 19:57:01 +0100

    re-enabling WS2812B support for master

--- Firmware/tinyK22_Master/source/platform.h
+++ Firmware/tinyK22_Master/source/platform.h
@@ -17,10 +17,17 @@
 #define PL_CONFIG_BOARD_ID_CLOCK_K02FN128     (5)  /* Board V1.4, dual clock with K02FN128 */
 #define PL_CONFIG_BOARD_ID_MASTER_LPC845_BRK  (6)  /* Board LPC-845-BRK breadboard as master */
 
+/* notes:
+ * 60 Billion configuration
+ * PL_CONFIG_BOARD_MASTER_K22_WS2812B disabled
+ * predefined configuration to 60 billion
+ * NVMC disabled
+ */
+
 /* select the board used: */
 #define PL_CONFIG_BOARD_ID         (PL_CONFIG_BOARD_ID_MASTER_K22FN512)
 
-#define PL_CONFIG_BOARD_MASTER_K22_WS2812B    (0 && PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_MASTER_K22FN512)  /* if it is the K22 master controlling the LEDs directly */
+#define PL_CONFIG_BOARD_MASTER_K22_WS2812B    (1 && PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_MASTER_K22FN512)  /* if it is the K22 master controlling the LEDs directly */
 
 /* CPU/Board selection: only one can be active! */
 #define PL_CONFIG_IS_LPC845        (McuLib_CONFIG_CPU_IS_LPC)     /* LPC845 */
@@ -40,7 +47,7 @@
 #elif 1 /* McuOneEclipse '60 billion lights' configuration: LPC845 boards (4 clocks), total 60 clocks, with RGB ring controlled by tinyK22 */
   #define PL_MATRIX_CONFIG_IS_8x3    (0 && PL_CONFIG_IS_MASTER) /* original 8x3 matrix configuration with 24 clocks */
   #define PL_MATRIX_CONFIG_IS_12x5   (1 && PL_CONFIG_IS_MASTER) /* new 8x3 matrix configuration with 60 clocks */
-  #define PL_MATRIX_CONFIG_IS_RGB    (0 && PL_CONFIG_IS_MASTER) /* if matrix has RGB rings */
+  #define PL_MATRIX_CONFIG_IS_RGB    (1 && PL_CONFIG_IS_MASTER) /* if matrix has RGB rings */
   #define PL_CONFIG_IS_NEW_MODULAR   (0) /* new modular boards with power-off switch */
 #elif 1 /* new 60 'modular' clock configuration */
   #define PL_MATRIX_CONFIG_IS_8x3    (0 && PL_CONFIG_IS_MASTER) /* original 8x3 matrix configuration with 24 clocks */
@@ -74,6 +81,12 @@
   #define PL_CONFIG_NOF_CLOCK_ON_BOARD_X   (4)
   #define PL_CONFIG_NOF_CLOCK_ON_BOARD_Y   (1)
   #define PL_CONFIG_NOF_CLOCK_ON_BOARD_Z   (2)
+#elif PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_MASTER_K22FN512 && PL_CONFIG_BOARD_MASTER_K22_WS2812B
+  /* virtual steppers for LED rings: settings for a virtual LPC845_Clock4 board */
+  #define PL_CONFIG_NOF_CLOCK_ON_BOARD     (4)
+  #define PL_CONFIG_NOF_CLOCK_ON_BOARD_X   (4)
+  #define PL_CONFIG_NOF_CLOCK_ON_BOARD_Y   (1)
+  #define PL_CONFIG_NOF_CLOCK_ON_BOARD_Z   (2)
 #elif PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_MASTER_K22FN512 /* dummy entries only */
   #define PL_CONFIG_NOF_CLOCK_ON_BOARD     (0)
   #define PL_CONFIG_NOF_CLOCK_ON_BOARD_X   (0)
@@ -93,7 +106,7 @@
 #define PL_CONFIG_USE_RTT           	(1 && (PL_CONFIG_IS_CLIENT || PL_CONFIG_IS_TINYK22)) /* use SEGGER RTT (only possible with a J-Link */
 #define PL_CONFIG_USE_RTC           	(1 && (PL_CONFIG_IS_MASTER || PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_CLOCK_K02FN128)) /* 1: enable RTC; 0: disable it */
 #define PL_CONFIG_USE_RS485         	(1 && PL_CONFIG_USE_SHELL) /* RS-485 connection, 1: enabled, 0: disabled: it requires the shell to parse the commands */
-#define PL_CONFIG_USE_NVMC          	(1) /* using non-volatile configuration memory */
+#define PL_CONFIG_USE_NVMC          	(0) /* using non-volatile configuration memory */
 #define PL_CONFIG_USE_WDT           	(0) /* if using watchdog timer, disable for easier debugging */
 #define PL_CONFIG_USE_DEMOS         	(1 && PL_CONFIG_USE_MATRIX && ((PL_CONFIG_IS_MASTER && PL_CONFIG_USE_RS485) || PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_CLOCK_K02FN128)) /* if using demos or not */
 #define PL_CONFIG_USE_BLE           	(1 && PL_CONFIG_IS_TINYK22) /* if using BLE or not */
@@ -103,7 +116,8 @@
 #define PL_CONFIG_USE_NEO_PIXEL     	(1 && (PL_CONFIG_BOARD_MASTER_K22_WS2812B || PL_CONFIG_IS_K02)) /* 1: using NeoPixels/WS2812B */
 #define PL_CONFIG_USE_MOTOR_ON_OFF    (1 && (PL_CONFIG_IS_NEW_MODULAR || PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_CLOCK_K02FN64 || PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_CLOCK_K02FN128)) /* using hardware to turn off/on the stepper motors to reduce power */
 
-#define PL_CONFIG_USE_STEPPER         (1 && (PL_CONFIG_IS_CLIENT || PL_CONFIG_BOARD_MASTER_K22_WS2812B)) /* enable stepper function, both motors and virtual (LED) stepper */
+#define PL_CONFIG_USE_VIRTUAL_STEPPER (PL_CONFIG_BOARD_MASTER_K22_WS2812B)
+#define PL_CONFIG_USE_STEPPER         (1 && (PL_CONFIG_IS_CLIENT || PL_CONFIG_USE_VIRTUAL_STEPPER)) /* enable stepper function, both motors and virtual (LED) stepper */
 #define PL_CONFIG_USE_LED_STEPPER     (1 && PL_CONFIG_USE_STEPPER && PL_CONFIG_USE_NEO_PIXEL && PL_CONFIG_IS_TINYK22) /* virtual LED Stepper without real stepper motor */
 #define PL_CONFIG_USE_X12_STEPPER     (1 && PL_CONFIG_USE_STEPPER && (PL_CONFIG_IS_K02 || PL_CONFIG_IS_LPC845)) /* if X12 stepper motors are used */
 #define PL_CONFIG_USE_X12_LED_STEPPER (PL_CONFIG_USE_X12_STEPPER && PL_CONFIG_USE_NEO_PIXEL)
@@ -115,7 +129,7 @@
 /* client only: */
 #define PL_CONFIG_USE_MAG_SENSOR      (1 && PL_CONFIG_IS_CLIENT)      /* has magnets and hall sensors */
 #define PL_CONFIG_USE_LED_DIMMING     (0 && PL_CONFIG_USE_LED_RING)
-#define PL_CONFIG_USE_DUAL_HANDS      (0 && PL_CONFIG_USE_LED_RING)   /* dual hand on Z axis */
+#define PL_CONFIG_USE_DUAL_HANDS      (1 && PL_CONFIG_USE_LED_RING)   /* dual hand on Z axis */
 #define PL_CONFIG_USE_AUTOMATIC_DEMO_MODE  (0 && PL_CONFIG_IS_CLIENT) /* play automatic demo after power-on */
 
 /* master only: */
