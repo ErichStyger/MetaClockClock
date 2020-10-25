6f2708bb6744bda08d5c8bd7f0d19e033559b344
Erich Styger <erich.styger@hslu.ch>
2020-10-25 08:19:01 +0100

    ability to stip of time in log

--- Firmware/LPC845_Clock4/source/IncludeMcuLibConfig.h
+++ Firmware/LPC845_Clock4/source/IncludeMcuLibConfig.h
@@ -92,6 +92,7 @@ Instructions:
 /* McuLog */
 #define McuLog_CONFIG_IS_ENABLED                      (1)
 #define McuLog_CONFIG_LOG_TIMESTAMP_DATE              (0)
+#define McuLog_CONFIG_LOG_TIMESTAMP_TIME              (0)
 #define McuLog_CONFIG_USE_RTT_DATA_LOGGER             (0)
 #define McuLog_CONFIG_RTT_DATA_LOGGER_BUFFER_SIZE     (128)
 /* -------------------------------------------------*/
