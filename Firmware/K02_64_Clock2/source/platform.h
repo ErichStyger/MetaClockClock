603b1e273f45b752a879e97c38911199fe847967
Erich Styger <erich.styger@hslu.ch>
2020-10-26 15:34:23 +0100

    compile with NVMC enabled

--- Firmware/K02_64_Clock2/source/platform.h
+++ Firmware/K02_64_Clock2/source/platform.h
@@ -99,7 +99,7 @@
 #define PL_CONFIG_USE_RTT           	(1 && (PL_CONFIG_IS_CLIENT || PL_CONFIG_IS_TINYK22)) /* use SEGGER RTT (only possible with a J-Link */
 #define PL_CONFIG_USE_RTC           	(1 && (PL_CONFIG_IS_MASTER || PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_CLOCK_K02FN128)) /* 1: enable RTC; 0: disable it */
 #define PL_CONFIG_USE_RS485         	(1 && PL_CONFIG_USE_SHELL) /* RS-485 connection, 1: enabled, 0: disabled: it requires the shell to parse the commands */
-#define PL_CONFIG_USE_NVMC          	(0) /* using non-volatile configuration memory */
+#define PL_CONFIG_USE_NVMC          	(1) /* using non-volatile configuration memory */
 #define PL_CONFIG_USE_WDT           	(0) /* if using watchdog timer, disable for easier debugging */
 #define PL_CONFIG_USE_DEMOS         	(1 && PL_CONFIG_USE_MATRIX && ((PL_CONFIG_IS_MASTER && PL_CONFIG_USE_RS485) || PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_CLOCK_K02FN128)) /* if using demos or not */
 #define PL_CONFIG_USE_BLE           	(1 && PL_CONFIG_IS_TINYK22) /* if using BLE or not */
