a3e66515409770317b5e6b042a3d87d6f390ac52
Erich Styger <erich.styger@hslu.ch>
2020-12-30 13:46:41 +0100

    after an intermezzo show the clock immediately again.

--- Firmware/ClockCommon/clock.c
+++ Firmware/ClockCommon/clock.c
@@ -505,6 +505,7 @@ static void ClockTask(void *pv) {
     #if PL_CONFIG_USE_INTERMEZZO
       if (!intermezzoShown) { /* show intermezzo? */
         INTERMEZZO_Play(lastClockUpdateTickCount, &intermezzoShown);
+        prevClockUpdateTimestampSec = 0; /* if there is time after the intermezzo: trigger showing clock again */
       }
     #endif
     } /* if clock is on */
