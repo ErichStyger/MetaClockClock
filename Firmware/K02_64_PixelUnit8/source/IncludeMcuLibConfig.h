220a62fa5352b6d292185643c2c14b0cf0952b71
Erich Styger <erich.styger@hslu.ch>
2021-01-06 18:34:49 +0100

    fixed comment

--- Firmware/K02_64_PixelUnit8/source/IncludeMcuLibConfig.h
+++ Firmware/K02_64_PixelUnit8/source/IncludeMcuLibConfig.h
@@ -32,7 +32,7 @@ Instructions:
 /* ------------------- SDK/Library ---------------------------*/
 #define McuLib_CONFIG_SDK_VERSION_USED   McuLib_CONFIG_SDK_MCUXPRESSO_2_0
 #define McuLib_CONFIG_CPU_IS_LPC         (0)  /* LPC845 */
-#define McuLib_CONFIG_CORTEX_M           (4)  /* K22 is a Cortex-M4 */
+#define McuLib_CONFIG_CORTEX_M           (4)  /* K02 is a Cortex-M4 */
 #define McuLib_CONFIG_CPU_IS_KINETIS     (1)  /* K02 is a Kinetis */
 
 /* ------------------- RTOS ---------------------------*/
