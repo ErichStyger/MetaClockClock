0bbd4f2039aebc82b8be2dbb9b0e4eddfbf06618
Erich Styger <erich.styger@hslu.ch>
2021-12-20 06:34:04 +0100

    added settings for Urs doing demo

--- Firmware/tinyK22_Master/source/platform.h
+++ Firmware/tinyK22_Master/source/platform.h
@@ -33,12 +33,17 @@
   #define PL_CONFIG_MATRIX_DEFAULT_HAND_COLOR       (0xFF0000)
   #define PL_CONFIG_MATRIX_DEFAULT_HAND_BRIGHTNESS  (0x50)
   #define PL_CONFIG_CLOCK_RANDOM_COLOR_ON           (1) /* if clock hands are enabled with random color*/
-#elif 0 /* V4: 24 clock version with round PCBs */
-  #define PL_MATRIX_CONFIGURATION_ID      PL_MATRIX_ID_CLOCK_8x3_V4
-  #define PL_CONFIG_USE_MOTOR_ON_OFF      (1) /* has motor on/off hardware */
-  #define PL_CONFIG_USE_LED_RING          (1) /* we do have LED rings */
-  #define PL_CONFIG_USE_EXTENDED_HANDS    (0) /* using extended hand */
-  //#define PL_CONFIG_USE_ESP32_UART        (1) /* using the 'ESP32' UART for the shell */
+#elif 1 /* V4: 24 clock version with round PCBs: wooden frame on walnut veneer */
+  #define PL_MATRIX_CONFIGURATION_ID                PL_MATRIX_ID_CLOCK_8x3_V4
+  #define PL_CONFIG_USE_MOTOR_ON_OFF                (1) /* has motor on/off hardware */
+  #define PL_CONFIG_USE_LED_RING                    (1) /* we do have LED rings */
+  #define PL_CONFIG_USE_EXTENDED_HANDS              (0) /* using extended hand */
+  #define PL_CONFIG_CLOCK_ON_BY_DEFAULT             (1) /* clock on by default */
+  #define PL_CONFIG_INTERMEZZO_ON_BY_DEFAULT        (1) /* intermezzos on by default */
+  #define PL_CONFIG_MATRIX_DEFAULT_HAND_COLOR       (0xFF0000)
+  #define PL_CONFIG_MATRIX_DEFAULT_HAND_BRIGHTNESS  (0x50)
+  #define PL_CONFIG_CLOCK_RANDOM_COLOR_ON           (1) /* if clock hands are enabled with random color*/
+  //#define PL_CONFIG_USE_ESP32_UART           (1) /* using the 'ESP32' UART for the shell */
 #elif 0 /* Alexis clock */
   #define PL_MATRIX_CONFIGURATION_ID      PL_MATRIX_ID_CLOCK_16x9_ALEXIS
   #define PL_CONFIG_USE_ESP32_UART        (1) /* using the 'ESP32' UART for the shell */
@@ -50,7 +55,7 @@
   #define PL_CONFIG_USE_CLOCK             (0) /* only implemented for dual shaft motors */
   #define PL_CONFIG_USE_FONT              (0) /* only implemented for dual shaft motors */
   #define PL_CONFIG_USE_LED_PIXEL         (1) /* pixels */
-#elif 0 /* circular clock */
+#elif 0 /* circular clock: 12 for each digit plus on in the center */
   #define PL_CONFIG_HAS_CIRCLE_CLOCK      (1)
   #define PL_MATRIX_CONFIGURATION_ID      PL_MATRIX_ID_CIRCULAR_CLOCK_1x12
   #define PL_CONFIG_USE_MOTOR_ON_OFF      (1) /* has motor on/off hardware */
