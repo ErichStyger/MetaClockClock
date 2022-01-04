6347df9ee4f44d8f62edb01de51abe89441135d0
Erich Styger <erich.styger@hslu.ch>
2022-01-04 16:44:25 +0100

    covering 2x2 configuration

--- Firmware/LPC845_Clock4/source/platform.h
+++ Firmware/LPC845_Clock4/source/platform.h
@@ -13,14 +13,16 @@
 #include "platform_id.h"
 
 /* select the board used: */
-#define PL_CONFIG_BOARD_ID            (PL_CONFIG_BOARD_ID_CLOCK_LPC845_1X4)
-
+#if 0 /* 60 billion time */
+  #define PL_CONFIG_BOARD_ID            (PL_CONFIG_BOARD_ID_CLOCK_LPC845_1X4)
+#else /* 'world stepper clock */
+  #define PL_CONFIG_BOARD_ID            (PL_CONFIG_BOARD_ID_CLOCK_LPC845_2X2)
+#endif
 /* ********************************************************************* */
 /* override settings in platform_common.h: */
 #define PL_CONFIG_USE_MAG_SENSOR        (1) /* if using magnets */
 
 #include "platform_common.h"
-
 
 void PL_InitFromTask(void);
 void PL_Init(void);
