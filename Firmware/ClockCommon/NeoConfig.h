eb5aa2220f27f477c030d4e1924b28bc38ee8676
Erich Styger <erich.styger@hslu.ch>
2022-05-29 18:46:45 +0200

    updates for pixel units

--- Firmware/ClockCommon/NeoConfig.h
+++ Firmware/ClockCommon/NeoConfig.h
@@ -18,12 +18,14 @@
 #elif PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_PIXELUNIT_K02FN64 || PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_PIXELUNIT_K02FN128
   #define NEOC_LANE_START        (5) /* this is the first GPIO pin used */
   #define NEOC_LANE_END          (5) /* this is the last GPIO pin used */
-  #define NEOC_NOF_LEDS_IN_LANE  (2*8) /* number of LEDs in a lane */
+  #define NEOC_NOF_LEDS_IN_COL   (1) /* 8 LEDs for each board */
+  #define NEOC_NOF_LEDS_IN_ROW   (8) /* each row/board has 16 LEDs */
+  #define NEOC_NOF_LEDS_IN_LANE  (NEOC_NOF_LEDS_IN_COL*NEOC_NOF_LEDS_IN_ROW) /* number of LEDs in a lane */
 #elif PL_CONFIG_BOARD_ID==PL_CONFIG_BOARD_ID_MASTER_K22FN512
   #if PL_MATRIX_CONFIGURATION_ID==PL_MATRIX_ID_SMARTWALL_16x5
     #define NEOC_LANE_START        (0) /* this is the first GPIO pin used */
     #define NEOC_LANE_END          (0) /* this is the last GPIO pin used */
-    #define NEOC_NOF_LEDS_IN_COL   (PL_CONFIG_NOF_DRIVER_BOARDS) /* have 5 rows with 16 LEDs each */
+    #define NEOC_NOF_LEDS_IN_COL   (PL_CONFIG_NOF_DRIVER_BOARDS) /* have the rows (boards) stacked with 16 LEDs each */
     #define NEOC_NOF_LEDS_IN_ROW   (16) /* each row/board has 16 LEDs */
     #define NEOC_NOF_LEDS_IN_LANE  (NEOC_NOF_LEDS_IN_COL*NEOC_NOF_LEDS_IN_ROW) /* number of LEDs, all in one line */
   #elif PL_MATRIX_CONFIGURATION_ID==PL_MATRIX_ID_CLOCK_12x5_60B /* 60 billion lights, with master behind the canvas driving the LEDs directly */
@@ -31,7 +33,7 @@
     #define NEOC_LANE_END          (4) /* this is the last GPIO pin used */
     #define NEOC_NOF_LEDS_IN_LANE  (12*40) /* number of LEDs in each lane */
   #else
-    #error "unknown konfiguration?"
+    #error "unknown configuration?"
   #endif
 #else
   #error "unknown"
