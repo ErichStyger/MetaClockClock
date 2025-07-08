ba6f494f06d28d287383c1b77f37098def7f6707
Erich Styger <erich.styger@hslu.ch>
2025-07-08 17:19:30 +0200

    ability to disable UART

--- Firmware/ClockCommon/platform_common.h
+++ Firmware/ClockCommon/platform_common.h
@@ -229,7 +229,9 @@
 #endif
 
 /* master only: */
-#define PL_CONFIG_USE_SHELL_UART    (1 && PL_CONFIG_IS_MASTER) /* using UART for USB-CDC to host (USB bridge on tinyK22 and LPC845-BRK) */
+#ifndef PL_CONFIG_USE_SHELL_UART
+  #define PL_CONFIG_USE_SHELL_UART    (1 && PL_CONFIG_IS_MASTER) /* using UART for USB-CDC to host (USB bridge on tinyK22 and LPC845-BRK) */
+#endif
 #ifndef PL_CONFIG_USE_ESP32_UART
   #define PL_CONFIG_USE_ESP32_UART   (0)  /* using an extra UART (to one on the tinyK22 board which is supposed to be for the ESP32 */
 #endif
